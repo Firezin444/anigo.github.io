f5dd40aff82f9ba619931c8dd609bedeeefbb7ac
Alvaro <101476179+anigo1@users.noreply.github.com>
2023-08-30 19:30:30 -0300

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,23 @@
+if (window.XMLHttpRequest){
+    xmlhttp = new XMLHttpRequest();
+} else {
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+}
+xmlhttp.open("GET", "../animes/lista.xml", false);
+xmlhttp.send();
+xmlDoc = xmlhttp.responseXML;
+
+document.write("<table border='1'>");
+let x = xmlDoc.getElementsByTagName("CD");
+for (let i = 0; i < x.length; i++){
+    document.write("<tr><td>");
+    document.write("Artista: " + x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue);
+    document.write("</br>")
+    document.write("Disco: " + x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue);
+    document.write("</td></tr>");
+}
+document.write("</table>");
+
 function start() {
     
 }
